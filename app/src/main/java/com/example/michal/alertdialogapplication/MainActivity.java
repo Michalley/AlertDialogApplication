8a4d269107b18889dab8c7a9a52eb7a1cb82a5d1
Michalley <ml5877@bs.amalnet.k12.il>
2018-12-01 20:28:36 +0200

    2nd commit

--- app/src/main/java/com/example/michal/alertdialogapplication/MainActivity.java
+++ app/src/main/java/com/example/michal/alertdialogapplication/MainActivity.java
@@ -5,8 +5,12 @@ import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -118,5 +122,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void btn5(View view) {
         ad5.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Credits");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this,"Application Of Michal Leyovich",Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
     }
 }
